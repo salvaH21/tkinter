cea35c06a8c73f67536f937fb7341b4bad554072
salvaH21 <sholivares90@gmail.com>
2023-11-25 10:08:58 +0100

    actualizacion

--- 003 - Tk/005 - Campo de entrada de texto.py
+++ 003 - Tk/005 - Campo de entrada de texto.py
@@ -4,4 +4,12 @@ raiz = tk.Tk()
 
 tk.Entry(raiz).pack(padx=50,pady=50)
 
+##FUNCIONES
+##entrada = tk.Entry(raiz)
+##entrada.pack()
+##entrada.get()-----OBITENE EL CONTENIDO DEL CAMPO (NORMALMENTE VA CON BUTTON)
+##entrada.insert(índice,string)-----PONE CONTENIDO EN EL CAMPO
+##entrada.delete(0,END)-----LIMPIA EL CAMPO
+
+
 raiz.mainloop()
